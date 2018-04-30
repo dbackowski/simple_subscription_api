8e895c4b656acdc11ebf04cccf11e84a6a4f29f3
Damian Baćkowski <damianbackowski@gmail.com>
2018-04-30 10:26:40 +0200

    fix set_next_billing_date callback in subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,11 +9,11 @@ class Subscription < ApplicationRecord
   validates :country,
     presence: true
 
-  after_save :set_next_billing_date
+  before_save :set_next_billing_date
 
   private
 
   def set_next_billing_date
-    self.next_billing_date = created_at + 1.month
+    self.next_billing_date = Time.zone.today + 1.month
   end
 end
